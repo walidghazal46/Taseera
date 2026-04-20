e65cacf489767f4ed024a80d2fe41f97f24bbd99
Walid Ghazal <walidghazal46@gmail.com>
2026-04-20 23:38:18 +0300

    Bump Android version to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.taseera.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "2"
+        versionCode = 3
+        versionName = "3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
