f4f06ab8915b9f1f1a72bcc076d67573f1b97e39
Walid Ghazal <walidghazal46@gmail.com>
2026-05-10 13:55:35 +0300

    Release: Version 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.taseera.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "15"
+        versionCode = 16
+        versionName = "16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
